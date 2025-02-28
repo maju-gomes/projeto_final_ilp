8dfe847b629210fb25e413dadca65f26c46ba383
maju <julia.gomes1@escolar.ifrn.edu.br>
2025-02-28 15:30:42 -0300

    recriei a lógica

--- arquivos_codigo/main.py
+++ arquivos_codigo/main.py
@@ -5,8 +5,19 @@ pygame.init()
 screen = pygame.display.set_mode((550, 650))
 pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
-
 fonte = pygame.font.Font("./assets/interface_do_usuario/pixelifsans.ttf", 40)
+# situacoes do jogo
+situacoes = {
+"inicio": 0,
+"jogando": 1,
+"derrota": 2}
+
+jogo_situacao = situacoes["inicio"] # O jogo começa desde o inicio
+
+velocidade_passaro_y = 0
+gravidade = 0.5
+posicao_passaro_y = 250
+impulso = -10
 
 # IMAGENS
 fundo_ceu = pygame.image.load("./assets/objetos/background-day.png")
@@ -18,44 +29,83 @@ base_rect= pygame.transform.scale(base_rect, (560, 112))
 passaro1_rect= pygame.image.load("./assets/objetos/yellowbird-midflap.png")
 passaro1_rect = pygame.transform.scale(passaro1_rect, (51, 36))
 
-# Variáveis do jogo
-passaro_y = 300  # Posição Y inicial do pássaro
-passaro_y_velocidade = 0  # Velocidade do movimento do pássaro
-gravidade = 0.5  # Força de gravidade que vai puxar o pássaro para baixo
-pulo = -10  # Velocidade do pulo do pássaro
-jogo_ativo = False  # Controle de início do jogo
 
-while True:
+
+# Lógica do jogo / Funções
+
+# jogo rodando
+def jogo_rodando():
+    global posicao_passaro_y, velocidade_passaro_y
+
+    # Aplica a gravidade e o movimento do pássaro
+    posicao_passaro_y += velocidade_passaro_y
+    velocidade_passaro_y += gravidade
+
+    screen.blit(fundo_ceu, (0, 0))  
+    screen.blit(base_rect, (0, 540))  
+    screen.blit(passaro1_rect,(225, posicao_passaro_y))  
+
+    if posicao_passaro_y >= 540:
+        # O pássaro atingiu o chão, ou seja: fim de jogo
+        return True
+
+    return False
+
+# dela de perda
+def tela_derrota():
+        # imagens 
+        
+
+        # textos
+        texto_derrota1 = fonte.render("Você perdeu!", True, (255, 255, 255))
+        texto_derrota2 = fonte.render("Clique para voltar", True, (255, 255, 255))
+        texto_derrota3 = fonte.render("à tela inicial", True, (255, 255, 255))
+        screen.blit(texto_derrota1, (155, 85))  # Exibe a mensagem de derrota
+        screen.blit(texto_derrota2, (75, 155))  # Exibe a mensagem de derrota
+        screen.blit(texto_derrota3, (75, 195))  # Exibe a mensagem de derrota
+
+def tela_inicio():
+        # imagens
+
+
+        # textos
+        texto_inicio = fonte.render("Clique para começar", True, (255, 255, 255))
+        screen.blit(texto_inicio, (75, 85))  # Exibe a mensagem de início
+
+# Loop principal
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            running = False
             pygame.quit()
             exit()
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  
-                jogo_ativo = True
-                passaro_y_velocidade = pulo 
+                if jogo_situacao == situacoes["inicio"]:
+                    jogo_situacao = situacoes["jogando"]
+                if jogo_situacao == situacoes["jogando"]:
+                    velocidade_passaro_y = impulso
 
-    if not jogo_ativo:
-        screen.blit(fundo_ceu, (0, 0))  
-        screen.blit(base_rect, (0, 540))  
-        texto = fonte.render("Clique para Começar", False, (225, 225, 255))
-        screen.blit(texto, (82,100))
-        screen.blit(passaro1_rect, (225, 250))
-    else:
-        passaro_y_velocidade += gravidade
-        passaro_y += passaro_y_velocidade
+# Condicionais 
+    if jogo_situacao == situacoes["inicio"]:
+        tela_inicio()
 
-        if passaro_y >= 540 - passaro1_rect.get_height():
-            jogo_ativo = False  # Finaliza o jogo
-            texto_perda = fonte.render("Você Perdeu", False, (255, 255, 255))
-            screen.blit(texto_perda, (82, 100))
+    elif jogo_situacao == situacoes["jogando"]:
+        if jogo_rodando():
+            jogo_situacao = situacoes["derrota"]
+        pygame.display.update()
 
+    elif jogo_situacao == situacoes["derrota"]:
+        tela_derrota()
 
-                
-        screen.blit(fundo_ceu, (0, 0))  
-        screen.blit(base_rect, (0, 540))  
-        screen.blit(passaro1_rect,(225, int(passaro_y)))  
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                # Reinicia o jogo
+                posicao_passaro_y = 250
+                velocidade_passaro_y = -1
+                jogo_situacao = situacoes["inicio"]  # Volta para o início
+        
     pygame.display.update()
     clock.tick(60)
